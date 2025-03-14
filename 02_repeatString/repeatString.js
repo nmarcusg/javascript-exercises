6f82064d61cc5c74e52e921fa7a7ff2fb3992a61
nmarcusg <89849200+nmarcusg@users.noreply.github.com>
2025-03-15 02:12:22 +0800

    solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(text, num) {
+    let repeated_string = "";
+    for (let i = 0; i < 3; i++) {
+        repeated_string += text;
+    }
 
+    return repeated_string; 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
